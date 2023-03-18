8546d4dbc1c90ba4c482fb1ad7ec4e02556bb7ae
dhruvsharma11 <93293451+dhruvsharma11@users.noreply.github.com>
2023-03-18 00:40:37 -0400

    initial commit

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+function LoginScreen(props) {
+    return (
+        <ImageBackground>
+
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default LoginScreen;
